a0e71ddc5104279925a784aab2f9611f6d4e572f
Max Linke <max.linke@checkmk.com>
2025-12-22 09:02:26 +0100

    chore: add run helper script
    Have a single script for common operatios so they are easier to pickup
    
    Change-Id: Ibf80967709c1fba21e536499625a4fcc03c57471

--- tests/test_cmk_dev_site.py
+++ tests/test_cmk_dev_site.py
@@ -6,14 +6,14 @@ from cmk_dev_site.omd import BaseVersion, VersionWithPatch, VersionWithReleaseDa
 
 def test_default_name_base_version():
     version = BaseVersion(2, 3, 0)
-    assert Config._default_name(version) == "v230"
+    assert Config._default_name(version) == "v230"  # pyright: ignore[reportPrivateUsage]
 
 
 def test_default_name_version_with_patch():
     version = VersionWithPatch(BaseVersion(2, 3, 0), "p", 1)
-    assert Config._default_name(version) == "v230p1"
+    assert Config._default_name(version) == "v230p1"  # pyright: ignore[reportPrivateUsage]
 
 
 def test_default_name_version_with_release_date():
     version = VersionWithReleaseDate(BaseVersion(2, 3, 0), date(2025, 1, 1))
-    assert Config._default_name(version) == "v230"
+    assert Config._default_name(version) == "v230"  # pyright: ignore[reportPrivateUsage]
