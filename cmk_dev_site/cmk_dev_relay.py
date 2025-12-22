c0735d708a700d923efa2ac59456adbeb0ce7a3d
Max Linke <max.linke@checkmk.com>
2025-12-22 09:08:53 +0100

    empty warpper cmk-relay
    Change-Id: Ic002620177db690cca37e0c677f1a3d896e393f8

---
+++ cmk_dev_site/cmk_dev_relay.py
@@ -0,0 +1,11 @@
+"""Relay tool for cmk-dev-site."""
+
+
+def main() -> int:
+    """Main entrypoint for cmk-dev-relay."""
+    print("hello relay")
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
